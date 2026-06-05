8bee6ff56aad0451f39d2c201a250519599b3207
Guramrit Bamrah <guramritb@gmail.com>
2026-06-04 21:59:44 -0400

    created models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    post_date = models.DateTimeField("date posted")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    description = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField('Tag')
+    def __str__(self):
+        return self.product_name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
