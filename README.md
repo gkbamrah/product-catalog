ad769bf5804b13ad97682dbdb6e4bb12a03f8c8f
Guramrit Bamrah <guramritb@gmail.com>
2026-06-05 21:49:57 -0400

    functional mvp. need to clean up a bit and fill out readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # remarcable-assessment
 A Django project to model products, tags, and categories.
+
+Note: db.sqlite3 is intentionally included in this repository as it contains the required sample data (5 categories, 10 tags, 20 products) as specified in the assignment requirements.
--- products/views.py
+++ products/views.py
@@ -1,16 +1,34 @@
-from django.db.models import F
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Product, Tag, Category
-from django.urls import reverse
-from django.views import generic
+from django.shortcuts import render
+from .models import Product
+from django.db.models import Q
+from .forms import ProductSearchForm
 
 
 def product_list(request):
     products = Product.objects.order_by("-post_date")
-    context = {"products": products}
-    print(request)
-    print("HELLOOO")
+
+    form = ProductSearchForm(request.GET)
+
+    
+    if form.is_valid():
+
+        search = form.cleaned_data["search"]
+        category = form.cleaned_data["category"]
+        tags = form.cleaned_data["tags"]
+
+        if search:
+            products = products.filter(Q(name__icontains=search) | Q(description__icontains=search))
+
+        if category:
+            products = products.filter(category=category)
+
+        if tags:
+            products = products.filter(tags__in=tags).distinct()
+
+    context = {
+                "products": products,
+                "form": form
+                }
     return render(request, "products/product-list.html", context)
 
 
