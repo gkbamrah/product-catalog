cf05d4a5f7448867434ca1abfff52d9384c02e4e
Guramrit Bamrah <guramritb@gmail.com>
2026-06-05 20:00:29 -0400

    webpage is more populated, now has search bar and dropdown for categories

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Product, Tag, Category
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.order_by("-post_date")
+    context = {"products": products}
+    print(request)
+    print("HELLOOO")
+    return render(request, "products/product-list.html", context)
+
+
