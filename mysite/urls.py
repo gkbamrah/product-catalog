7f9fe355f9c73d18324d28e628c28dec127be493
Guramrit Bamrah <guramritb@gmail.com>
2026-07-04 18:27:28 -0400

    code clean up

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("", include("products.urls")),
     path("admin/", admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -4,36 +4,8 @@ from rest_framework.response import Response
 from django.shortcuts import render
 from .models import Category, Product
 from django.db.models import Q
-from .forms import ProductSearchForm
 from .serializers import CategorySerializer, ProductSerializer
 
-
-def product_list(request):
-    products = Product.objects.order_by("-post_date")
-
-    form = ProductSearchForm(request.GET)
-
-    
-    if form.is_valid():
-
-        search = form.cleaned_data["search"]
-        category = form.cleaned_data["category"]
-        tags = form.cleaned_data["tags"]
-
-        if search:
-            products = products.filter(Q(name__icontains=search) | Q(description__icontains=search))
-
-        if category:
-            products = products.filter(category=category)
-
-        if tags:
-            products = products.filter(tags__in=tags).distinct()
-
-    context = {
-                "products": products,
-                "form": form
-                }
-    return render(request, "products/product-list.html", context)
 
 @api_view(["GET"])
 def product_list_api(request):
