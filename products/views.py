c973a7a49e45bb935d0fe9d0360adece9921335d
Guramrit Bamrah <guramritb@gmail.com>
2026-07-13 20:32:29 -0400

    created endpoint for POST request to create products

--- products/views.py
+++ products/views.py
@@ -7,25 +7,33 @@ from django.db.models import Q
 from .serializers import CategorySerializer, ProductSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def product_list_api(request):
-    products = Product.objects.order_by("-post_date")
     
-    search = request.GET.get("search")
-    category = request.GET.get("category")
-    tags = request.GET.getlist("tags")
-    
-    if search:
-        products = products.filter(Q(name__icontains=search) | Q(description__icontains=search))
-
-    if category:
-        products = products.filter(category=category)
-
-    if tags:
-        products = products.filter(tags__in=tags).distinct()
+    if request.method == "GET":
+        products = Product.objects.order_by("-post_date")
         
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        search = request.GET.get("search")
+        category = request.GET.get("category")
+        tags = request.GET.getlist("tags")
+        
+        if search:
+            products = products.filter(Q(name__icontains=search) | Q(description__icontains=search))
+
+        if category:
+            products = products.filter(category=category)
+
+        if tags:
+            products = products.filter(tags__in=tags).distinct()
+            
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET"])
 def category_list_api(request):
